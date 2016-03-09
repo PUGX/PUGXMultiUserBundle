f8d08549fb487dc29fd97f684cb2cbc2db783d32
Pablo Díaz Márquez <fcpauldiaz@users.noreply.github.com>
2016-03-09 14:31:37 -0600

    Update ProfileManager.php
    I had to do this to make the show action to work. I'm not sure if this is the right way to use the show action.

--- Controller/ProfileManager.php
+++ Controller/ProfileManager.php
@@ -6,8 +6,11 @@ use PUGX\MultiUserBundle\Form\FormFactory;
 use PUGX\MultiUserBundle\Model\UserDiscriminator;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ProfileManager
+class ProfileManager extends Controller
 {
     /**
      * @var UserDiscriminator
@@ -71,11 +74,41 @@ class ProfileManager
             'form' => $form->createView(),
         ));
     }
+    
+    
+    /**
+     * @param string $class
+     * @return RedirectResponse
+     */
+    public function show($class)
+    {
+        $user =  $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $this->userDiscriminator->setClass($class);
+
+        $this->controller->setContainer($this->container);
+        $result = $this->controller->showAction($this->getRequest());
+        if ($result instanceof RedirectResponse) {
+            return $this->controller->redirect($this->getRequest()->getRequestUri());
+        }
+
+        $template = $this->userDiscriminator->getTemplate('profile');
+        if (is_null($template)) {
+            $template = 'FOSUserBundle:Profile:show.html.twig';
+        }
+       
+        return $this->container->get('templating')->renderResponse($template, array(
+            'user' => $user
+        ));
+    }
 
     /**
      * @return \Symfony\Component\HttpFoundation\Request;
      */
-    private function getRequest()
+    public function getRequest()
     {
         return $this->container->get('request_stack')->getCurrentRequest();
     }
